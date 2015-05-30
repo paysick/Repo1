23d3f7e6e83dd0169540797dca7ca6a8f85fbcca
paysick <joshk.bits@gmail.com>
2015-05-30 14:33:31 +0530

    Update BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -3,14 +3,14 @@ public class BMICalculator
 {
 	public static void main(String[] args)
 	{
-		double kg,m,BMI;
+		int kg,m,BMI;
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.print("what is your weight in kgs? ");
-		kg = keyboard.nextDouble();
+		kg = keyboard.nextInt();
 		
 		System.out.print("what is your height in meters? ");
-		m = keyboard.nextDouble();
+		m = keyboard.nextInt();
 		
 		BMI = kg/(m*m);
 		
